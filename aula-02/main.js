8f16969e9fde1f71b1b6051fea4d230db096b9b2
LAYLA GIOVANNA FURTADO DA SILVA <22282096@CFP127ALUNO.LOCAL>
2023-02-16 11:27:53 -0300

    aula-02 quase pronta

--- aula-02/main.js
+++ aula-02/main.js
@@ -3,25 +3,50 @@
 //usar esse jeito apenas se eu nn irei iteragir novamente com essa variavel
 
 const adicionar = document.getElementById('add')
+let novaDiv;
 
 /* const adicionarCard = () => {
      const container = document.getElementById('container')
      container.innerHTML= container.innerHTML + '<div class="items"></div>'
 } */
 
-const adicionarCard = (aluno) => {
+const adicionarCard = (aluno, nota) => {
     const container = document.getElementById('container')
+
     //create elements
-    const novaDiv = document.createElement('div')
+    novaDiv = document.createElement('div')
     novaDiv.classList.add('aluno')
-    novaDiv.innerHTML = `<h2 class="aluno__title">${aluno}</h2>`
+    novaDiv.classList.add('nota')
+    novaDiv.innerHTML = `<h2 class="aluno__title">${aluno}</h2>
+                        <h2 class="nota__title">${nota}</h2>`
 
     container.appendChild(novaDiv)
     return 'teste'
+    
+}
+
+const handleClick = () => {
+    const nomeAluno = prompt('Digite seu nome')
+    const notaAluno = prompt('Digite sua nota')
+    adicionarCard(nomeAluno , notaAluno)
+
+    if(notaAluno <5){
+        novaDiv.classList.add('vermelho')
+    }else if (notaAluno >=5 && notaAluno <=10){
+        novaDiv.classList.add('verde')
+    }else if (notaAluno != isNaN){
+        alert("Por favor, digite um número.\nTente novamente.")
+        
+    }else {
+        return false
+    }
 
 }
 
 
+adicionar.addEventListener('click', handleClick)
+limpar.addEventListener('click', limpar)
 
+// SOLID - Responsividade unica
+//Funções puras
 
-adicionar.addEventListener('click', () => adicionarCard('Layla'))
